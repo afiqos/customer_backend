14833d73e2060ae1272a1cbebb36836145fbbf8c
afiqos <43640903+afiqos@users.noreply.github.com>
2020-08-13 22:01:51 +0800

    working findById test

--- src/test/kotlin/com/example/customerbackend/controller/CustomerControllerTest.kt
+++ src/test/kotlin/com/example/customerbackend/controller/CustomerControllerTest.kt
@@ -4,13 +4,17 @@ import com.example.customerbackend.entities.Customer
 import com.example.customerbackend.repositories.CustomerRepository
 import com.example.customerbackend.services.CustomerService
 import com.google.gson.Gson
+import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Bean
 import org.springframework.http.MediaType
 import org.springframework.test.context.jdbc.Sql
@@ -19,26 +23,32 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 
 @ExtendWith(SpringExtension::class)
-@WebMvcTest(CustomerController::class)
-@Sql("/schema-test.sql", "/import.sql")
+//@WebMvcTest(CustomerController::class)
+@SpringBootTest
+@AutoConfigureMockMvc
+//@Sql("/schema-test.sql", "/import.sql")
 internal class CustomerControllerTest {
 
-    @TestConfiguration
-    class CustomerControllerTestConfig {
-        @Bean
-        fun customerService() = mockk<CustomerService>(relaxed = true)
-
-        @Bean
-        fun CustomerRepository() = mockk<CustomerRepository>()
-    }
+//    @TestConfiguration
+//    class CustomerControllerTestConfig {
+//        @Bean
+//        fun customerService() = mockk<CustomerService>(relaxed = true)
+//
+//        @Bean
+//        fun CustomerRepository() = mockk<CustomerRepository>()
+//    }
 
     @Autowired
     private lateinit var mockMvc: MockMvc
 
-    @Autowired
-    private lateinit var customerService: CustomerService
+//    @Autowired
+//    private lateinit var customerService: CustomerService
+
+    private var customerService = mockk<CustomerService>()
+    private var customerRepository = mockk<CustomerRepository>()
 
     @Test
     fun springContextLoaded() {
@@ -49,7 +59,8 @@ internal class CustomerControllerTest {
     fun testFindById() {
 
         val testCustomerId = 1
-        val expectedResult = Customer(1, "WrongName", "Home", "user111", "pass111")
+//        val expectedResult = Customer(1, "Wrong name", "Homeee", "user111", "pass111")
+        val expectedResult = Customer(1, "Jack", "Home", "user111", "pass111")
 //        every { customerService.findById(testCustomerId) } returns expectedResult
 
         val gson = Gson()
@@ -58,12 +69,12 @@ internal class CustomerControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
 
                 .andExpect(status().isOk)
-//                .andExpect(content().json(gson.toJson(expectedResult)))
-                .andDo(print())
-                .andReturn()
+                .andExpect(content().json(gson.toJson(expectedResult)))
+//                .andDo(print())
+//                .andReturn()
 
-        print("kkkk " + result.response.contentAsString)
-        assertEquals(gson.toJson(expectedResult), result.response.contentAsString)
+//        print("kkkk " + result.response.contentAsString)
+//        assertEquals(gson.toJson(expectedResult), result.response.contentAsString)
     }
 
 
