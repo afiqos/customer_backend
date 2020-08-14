31c6fde931e393827a404dde78a3ce549c3ff735
afiqos <43640903+afiqos@users.noreply.github.com>
2020-08-14 14:15:58 +0800

    Changes to autoincrement id and no empty customer fields allowed

--- src/main/kotlin/com/example/customerbackend/entities/Customer.kt
+++ src/main/kotlin/com/example/customerbackend/entities/Customer.kt
@@ -1,27 +1,29 @@
 package com.example.customerbackend.entities
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
+import javax.validation.constraints.NotBlank
 
 @Entity
 @Table(name = "customers")
 data class Customer (
         @Id
         @Column(name = "account_number")  // not sure why if using "accountNumber" it will be defaulted to "account_number" later on
-        // also an option to make this autoincrement, but perhaps require changes in the data.sql, schema.sql too
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         var accountNumber: Int,
 
+        @field:NotBlank(message = "Customer's name is missing")
         @Column(name = "name")
         var name: String,
 
+        @field:NotBlank(message = "Customer's address is missing")
         @Column(name = "address")
         var address: String,
 
+        @field:NotBlank(message = "Customer's username is missing")
         @Column(name = "username")
         var username: String,
 
+        @field:NotBlank(message = "Customer's password is missing")
         @Column(name = "password")
         var password: String // probably need to be something else? maybe hashed or something
 ) {
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS customers;
 
 CREATE TABLE customers (
-    account_number INT PRIMARY KEY,
-    name VARCHAR(250) NOT NULL,
-    address VARCHAR(250) NOT NULL,
-    username VARCHAR(250) NOT NULL,
-    password VARCHAR(250) NOT NULL
+    account_number INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(250),
+    address VARCHAR(250),
+    username VARCHAR(250),
+    password VARCHAR(250)
 );
